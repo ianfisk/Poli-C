82c2ddbce0a23d7ca4dca4875f000e1d6a7b4e83
Ian Fisk <i.fisk32@gmail.com>
2018-09-12 02:07:29 -0700

    Add exports.

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as backoffs from './backoff-algorithms';
+
+export { RetryPolicy } from './retry-policy';
+export { CancellationTokenSource, CancellationToken } from './cancellation-token-source';
+export { backoffs };
